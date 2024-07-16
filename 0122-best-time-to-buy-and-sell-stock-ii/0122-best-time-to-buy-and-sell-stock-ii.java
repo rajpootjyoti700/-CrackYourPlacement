42a48b6cb450ab2c96a795521c3e3d012a09ff8d
Jyoti Rajpoot <133164017+rajpootjyoti700@users.noreply.github.com>
2024-07-16 12:50:45 +0530

    Time: 1 ms (89.90%), Space: 45.7 MB (46.01%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,12 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int max=0;
-        int n=prices.length;
+        // there is little diffrence in this ques from the previus question
+        // as we can buy and sell in multiple days 
+        //nd the same day when we will buy we can sell also;
+        
         int start=prices[0];
+        int n=prices.length;
+        int max=0;
         for(int i=1;i<n;i++){
             if(start<prices[i])
                 max=max+prices[i]-start;
-            
             start=prices[i];
         }
         return max;
