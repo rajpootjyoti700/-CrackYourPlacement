48288dada6b5d4d69b60e00c109a1ef2c0c7b2e2
Jyoti Rajpoot <133164017+rajpootjyoti700@users.noreply.github.com>
2024-07-18 13:22:11 +0530

    Time: 0 ms (100.00%), Space: 45.2 MB (10.49%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
@@ -5,7 +5,7 @@
  *     TreeNode left;
  *     TreeNode right;
  *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int v(root1.left,root2.left)al) { this.val = val; }
  *     TreeNode(int val, TreeNode left, TreeNode right) {
  *         this.val = val;
  *         this.left = left;
@@ -15,17 +15,14 @@
  */
 class Solution {
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-     // we will try to merge both  in a single  tree is that i 1st tree
-        // try from bootom up approch
         if(root1==null)
             return root2;
         if(root2==null)
             return root1;
+        //changing in root 1 itself
         root1.val=root1.val+root2.val;
         root1.left=mergeTrees(root1.left,root2.left);
         root1.right=mergeTrees(root1.right,root2.right);
         return root1;
-        
-        
     }
 }
