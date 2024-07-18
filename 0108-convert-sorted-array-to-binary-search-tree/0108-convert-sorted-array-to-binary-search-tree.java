f8d1afa629ff6d13b03f3adc896390f4a3fdfb7d
Jyoti Rajpoot <133164017+rajpootjyoti700@users.noreply.github.com>
2024-07-18 12:42:49 +0530

    Time: 0 ms (100.00%), Space: 43 MB (47.84%) - LeetHub

---
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        //As it is a bst so we can ssolve this like th mid element 
+        // should the root of the tree and from l to mid-1 should be the 
+        // left subtree  and
+        //mid +1 to r should be the right subtree 
+        /// so idea is binay search type of thing
+        
+       return  fun(nums,0,nums.length-1);
+    }
+    
+    public TreeNode fun(int[] nums, int l,int r){
+        if(l>r)
+            return  null;
+        int mid=l+(r-l)/2;
+        TreeNode root=new TreeNode(nums[mid]);
+        root.left=fun(nums,l,mid-1);
+        root.right=fun(nums,mid+1,r);
+        
+        return root;
+        
+    }
+}
