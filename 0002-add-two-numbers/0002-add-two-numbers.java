544aa63fd49d5ca0c818b4c20c1b14020a95936e
Jyoti Rajpoot <133164017+rajpootjyoti700@users.noreply.github.com>
2024-08-05 23:30:05 +0530

    Time: 1 ms (100.00%), Space: 44.4 MB (34.50%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy=new ListNode(0);
+        ListNode temp=dummy;
+        int carry=0;
+        int digit=0;
+        while(l1!=null || l2!=null || carry!=0){
+            if(l1!=null && l2!=null){
+                 digit=l1.val+l2.val+carry;
+                l1=l1.next;
+                l2=l2.next;
+            }
+            else if(l1!=null && l2==null){
+                digit=l1.val+carry;
+                l1=l1.next;
+            }
+            else if(l1==null && l2!=null){
+                digit=l2.val+carry;
+                l2=l2.next;
+            }
+            else if(l1==null && l2==null){
+                digit=carry;
+            }
+            carry=digit/10;
+            digit=digit%10;
+            ListNode curr=new ListNode(digit);
+            temp.next=curr;
+            temp=temp.next;
+        }
+        return dummy.next;
+    }
+}
