1ce3133407f8b55e00d88f9406452a1c11f36f9a
Jyoti Rajpoot <133164017+rajpootjyoti700@users.noreply.github.com>
2024-09-07 12:33:42 +0530

    Time: 2 ms (72.03%), Space: 61.9 MB (27.50%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,14 +1,22 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int buy = prices[0];
-        int profit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] < buy) {
-                buy = prices[i];
-            } else if (prices[i] - buy > profit) {
-                profit = prices[i] - buy;
+    // so the main conditions are we need max profit i e. greedy
+        // we have to buy 1st then sell 
+        // nd the selling elemeny must be grter then the buying elemt
+        int max=0;
+        int n=prices.length;
+         int buy=prices[0];
+        int profit=0;
+        for(int i=1;i<n;i++){
+                if(prices[i]<=buy)
+                    buy=prices[i];
+                 if(prices[i]>buy){
+                    profit=prices[i]-buy;
+                   max= Math.max(profit,max);
+                }
             }
-        }
-        return profit;
+        
+        return max;
     }
+    
 }
