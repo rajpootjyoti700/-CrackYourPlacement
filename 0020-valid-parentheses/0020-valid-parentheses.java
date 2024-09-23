e271b14211916aed447936aa890e06f2933a35b9
Jyoti Rajpoot <133164017+rajpootjyoti700@users.noreply.github.com>
2024-09-23 10:23:44 +0530

    Time: 2 ms (52.38%), Space: 41.1 MB (96.92%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,30 +1,49 @@
 class Solution {
     public boolean isValid(String s) {
        
+//     Stack<Character> st=new Stack<>();
+//         char[] arr=s.toCharArray();
+//         int n=arr.length;
+//         for(int i=0;i<n;i++)
+//         {
+//             if(arr[i]=='(' || arr[i]=='{' || arr[i]=='['){
+//                  st.push(arr[i]);
+//              }
+//             else{
+//                 if(st.isEmpty())
+//                     return false;
+//                else{
+                    
+//                    if((arr[i]==')' && st.peek()=='(') || (arr[i]=='}' && st.peek()=='{') || (arr[i]==']' && st.peek()=='[') ){
+//                         st.pop();
+//                     }
+//                    else
+//                        return false;
+                   
+//                 }
+//            }
+//        }
+//            return st.isEmpty();
+        // the no of open bracket and close baracke should be same 
+        
     Stack<Character> st=new Stack<>();
         char[] arr=s.toCharArray();
         int n=arr.length;
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]=='(' || arr[i]=='{' || arr[i]=='['){
-                 st.push(arr[i]);
-             }
+        for(int i=0;i<n;i++){
+            if(arr[i]=='(' || arr[i]=='{' || arr[i]=='[')
+                st.push(arr[i]);
             else{
                 if(st.isEmpty())
                     return false;
-               else{
-                    
-                   if((arr[i]==')' && st.peek()=='(') || (arr[i]=='}' && st.peek()=='{') || (arr[i]==']' && st.peek()=='[') ){
-                        st.pop();
-                    }
-                   else
-                       return false;
-                   
-                }
-           }
-       }
-           return st.isEmpty();
-        // the no of open bracket and close baracke should be same 
-        
+            
+            else{
+                if((arr[i]==')' && st.peek()=='(') || (arr[i]=='}' && st.peek()=='{' ) || (arr[i]==']' && st.peek()=='['))
+                    st.pop();
+                else
+                    return false;
+            }
+        }
+        }
+        return st.isEmpty();
         }
 }
