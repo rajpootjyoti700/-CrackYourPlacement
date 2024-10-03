b868a6ccc414624d5457d50ebb51dfb2ae484a77
Jyoti Rajpoot <133164017+rajpootjyoti700@users.noreply.github.com>
2024-10-03 13:19:01 +0530

    Time: 2 ms (89.61%), Space: 55.6 MB (86.92%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -1,46 +1,88 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        int n = bills.length;
-        int a = 0;
-        int b = 0;
-        int c = 0;
-        for (int i = 0; i < n; i++) {
-            if (bills[i] == 5) {
-                a++; // 8
-            }
-            if (bills[i] == 10) {
-                b++; // 1
-                if (a > 0) {
-                    a--;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            if (bills[i] == 20) {
-                c++;
-                if (a <= 0) {
-                    return false;
-                }
+//         int n = bills.length;
+//         int a = 0;
+//         int b = 0;
+//         int c = 0;
+//         for (int i = 0; i < n; i++) {
+//             if (bills[i] == 5) {
+//                 a++; // 8
+//             }
+//             if (bills[i] == 10) {
+//                 b++; // 1
+//                 if (a > 0) {
+//                     a--;
+//                 }
+//                 else
+//                 {
+//                     return false;
+//                 }
+//             }
+//             if (bills[i] == 20) {
+//                 c++;
+//                 if (a <= 0) {
+//                     return false;
+//                 }
 
-                if (b <= 0) {
-                    if (a >= 3) {
+//                 if (b <= 0) {
+//                     if (a >= 3) {
+//                         a=a-3;
+//                     }
+//                     else
+//                     {
+//                         return false;
+//                     }
+//                 }
+//                 if(b>0)
+//                 {
+//                     a--;
+//                     b--;
+//                 }
+//             }
+//         }
+
+//         return true;
+        
+    int a=0;
+    int b=0;
+    int c=0;
+        int n=bills.length;
+        for(int i=0;i<n;i++){
+            if(bills[i]==5)
+                a++;
+            if(bills[i]==10){
+                b++;
+                if(a>0){
+                    a=a-1;
+                }
+                else{
+                    return false;
+                }
+            }
+            
+            if(bills[i]==20){
+                c++;
+                if(a<=0){
+                    return false;
+                }
+                
+                if(b<=0){
+                    if(a>=3)
                         a=a-3;
-                    }
-                    else
-                    {
+                    else{
                         return false;
                     }
                 }
-                if(b>0)
-                {
-                    a--;
-                    b--;
-                }
+                    if(b>0){
+                        b--;
+                        a--;
+                    }
+                
             }
+            
+                
         }
-
+        
         return true;
     }
 }
