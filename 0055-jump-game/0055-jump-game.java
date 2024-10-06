fa591d05b429a30058a43ebccbecb95dadbca6f9
Jyoti Rajpoot <133164017+rajpootjyoti700@users.noreply.github.com>
2024-10-06 11:23:39 +0530

    Time: 1 ms (99.94%), Space: 46 MB (15.01%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,11 +1,16 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int targetNumIndex = nums.length - 1;
-        for (int i = nums.length - 2; i >= 0; i--) {
-            if (targetNumIndex <= i + nums[i]) {
-                targetNumIndex = i;
-            }
-        }
-        return targetNumIndex == 0;
+  // the approch is we can start traversing from the end
+        // and we  will the previous index +that element if greater 
+        //than obiosly it will reach the next step becoz it is the largest
+        // length
+int target=nums.length-1;
+    for(int i=nums.length-2;i>=0;i--){
+        if(target<=i+nums[i])//means we can reach the target
+            target=i;
+            
+    } 
+      return target==0;  
+        
     }
 }
